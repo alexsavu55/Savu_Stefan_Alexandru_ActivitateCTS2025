8c59e6d72e554754d10d12bf57e870d24a4563e5
Alex Savu <alexei.savu@gmail.com>
2025-05-11 09:49:58 +0300

    Seminar10( Composite, FlyWeight)

---
+++ ActivitateCTS_Savu_Stefan_Alexandru/Seminar10/src/Composite/Clase/IStructura.java
@@ -0,0 +1,8 @@
+package Composite.Clase;
+
+public interface IStructura {
+    void descrieStructura();
+    void adaugaNod(IStructura structura) throws Exception;
+    void stergeNod(IStructura structura) throws Exception;
+    IStructura getCopilNod(int index) throws Exception;
+}
---
+++ ActivitateCTS_Savu_Stefan_Alexandru/Seminar10/src/FlyWeight/Clase/IPacientAbstract.java
@@ -0,0 +1,5 @@
+package FlyWeight.Clase;
+
+public interface IPacientAbstract {
+    void afisareInformatii(Spitalizare s);
+}
